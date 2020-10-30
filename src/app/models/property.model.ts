052040baf2ddd0322453b9f8dcd5c23bb9a64fd3
Souleymane Mouctar Diallo <soulmouctar96@gmail.com>
2020-10-30 16:25:57 +0000

    ajout d'un model de données

---
+++ src/app/models/property.model.ts
@@ -0,0 +1,11 @@
+export class Property{
+  constructor(
+    public title : string,
+    public egory : string,
+    public face : string,
+    public ms : string,
+    public cription: string,
+    public ice:string,
+    public d : true
+  ){ }
+}
